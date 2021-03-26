2fa5496a2080505cabdc03b05f27ec304ba16b60
Giovanni <giovanni@gslab.it>
2021-03-26 01:38:40 +0100

    New CI Tests (#8) (#9)
    * 1st test
    
    * [ADD] l10n_it_fatturapa_in
    
    * pre-commit config
    
    * 2nd test
    
    * 3rd test
    
    * linting
    
    * added config files
    
    * more fixes
    
    * base_location
    
    * linting more
    
    * 4th test
    
    * makepot
    
    * fix makepot
    
    * 6th
    
    * 7th
    
    * 8th
    
    * 9th
    
    * 10th

--- base_location_italian_cities_import/models/res_city.py
+++ base_location_italian_cities_import/models/res_city.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class City(models.Model):
     _inherit = "res.city"
 
-    istat_code = fields.Char(string='ISTAT Code')
+    istat_code = fields.Char(string="ISTAT Code")
